55c669f746d30b907af5f29bb18aa682c8d337ab
Joshua Brockschmidt <JoshuaBrockschmidt@gmail.com>
2018-12-15 03:41:27 -0800

    Limit displayed categories in the shop menu
    Only display categories that contain valid tools

--- flood/gamemode/client/vgui/flood_shopmenu_tools.lua
+++ flood/gamemode/client/vgui/flood_shopmenu_tools.lua
@@ -17,14 +17,14 @@ function PANEL:Init()
 	self.List:SetWidth( 130 )
 	print(self.List)
 	self.List.Paint = function(self, w, h) 
-		draw.RoundedBox(0, 0, 0, w, h, Color(0, 0, 0, 0)) -- Inisibru
+		draw.RoundedBox(0, 0, 0, w, h, Color(0, 0, 0, 0))
 	end
 
 	self.Content = vgui.Create( "DCategoryList", self )
 	self.Content:Dock( FILL )
 	self.Content:DockMargin( 6, 0, 0, 0 )
 	self.Content.Paint = function(self, w, h)
-		draw.RoundedBox(0, 0, 0, w, h, Color(0, 0, 0, 0)) -- Inisibru
+		draw.RoundedBox(0, 0, 0, w, h, Color(0, 0, 0, 0))
 	end
 
 	if self.GmodTools then
@@ -38,37 +38,49 @@ function PANEL:Init()
 			end
 		end
 	else
-		LocalPlayer():ChatPrint("There has been an error loading your tools section, please rejoin the server or contact an administrator to fix this.")
+		LocalPlayer():ChatPrint("There has been an error loading your tools section, please rejoin the server or contact an administrator to resolve the issue.")
 	end
 end
 
 function PANEL:AddCategory(Name, Label, tItems)
-	local Category = self.List:Add(Label)
-	Category.Paint = function(self, w, h)
-		draw.RoundedBox(4, 0, 0, w, h, Color(240, 240, 240, 255))
-		draw.RoundedBoxEx(4, 0, 0, w, self.Header:GetTall(), Color(24, 24, 24, 255), true, true, false, false)
-	end
-	Category:SetCookieName("ToolMenu." .. tostring(Name))
-	for k, v in pairs(tItems) do
+	-- Find all valid tools for this category.
+	validTools = {}
+	for _, item in pairs(tItems) do
 		for _, tools in pairs(self.ToolTable) do
-			if tostring(v.ItemName) == tostring(tools[1]) then
+			if tostring(item.ItemName) == tostring(tools[1]) then
 				if tobool(tools[3]) == true then
-					local item = Category:Add(v.Text)
-					item.DoClick = function(button)
-						self:CreateCP(button)
-						LocalPlayer():ConCommand(v.Command)
-					end
-
-					item.ControlPanelBuildFunction = v.CPanelFunction
-					item.Command = v.Command
-					item.Name = v.ItemName
-					item.Controls = v.Controls
-					item.Text = v.Text
+					table.insert(validTools, item)
 				end
 			end
 		end
 	end
-	self:InvalidateLayout()
+
+	-- If there are valid tools, add this category and its tools to the panel.
+	-- Otherwise don't add a new category.
+	if next(validTools) ~= nil then
+		local Category = self.List:Add(Label)
+		Category.Paint = function(self, w, h)
+			draw.RoundedBox(4, 0, 0, w, h, Color(240, 240, 240, 255))
+			draw.RoundedBoxEx(4, 0, 0, w, self.Header:GetTall(), Color(24, 24, 24, 255), true, true, false, false)
+		end
+		Category:SetCookieName("ToolMenu." .. tostring(Name))
+
+		for _, tool in pairs(validTools) do
+			local item = Category:Add(tool.Text)
+			item.DoClick = function(button)
+				self:CreateCP(button)
+				LocalPlayer():ConCommand(tool.Command)
+			end
+
+			item.ControlPanelBuildFunction = tool.CPanelFunction
+			item.Command = tool.Command
+			item.Name = tool.ItemName
+			item.Controls = tool.Controls
+			item.Text = tool.Text
+		end
+		
+		self:InvalidateLayout()
+	end
 end
 
 function PANEL:CreateCP( button ) 
